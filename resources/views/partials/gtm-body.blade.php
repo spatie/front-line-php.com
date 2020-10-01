a519f0161ffd4e06c9628d4810d1ccf9744b0082
Brent Roose <brent@spatie.be>
2020-10-01 14:24:30 +0200

    GTM

--- resources/views/partials/gtm-body.blade.php
+++ resources/views/partials/gtm-body.blade.php
@@ -1,10 +1,9 @@
-<!-- Google Tag Manager (noscript) -->
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-171834166-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-171834166-2"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-171834166-1');
+    gtag('config', 'UA-171834166-2');
 </script>
