0db4db85f016edc07f009eb624c1d09dc1b3645e
freek <freek@spatie.be>
2021-11-19 09:14:00 +0100

    wip

--- resources/views/front/cheat-sheet/partials/enums.blade.php
+++ resources/views/front/cheat-sheet/partials/enums.blade.php
@@ -1,34 +1,34 @@
 <x-card id="enums" title="Enums" class="lg:col-span-2">
 
     <div class="grid lg:grid-cols-2 gap-6">
-        <x-card id="declaring-enums" title="Enums" level="2" php="8.1">
+        <x-card id="declaring-enums" title="Declaring Enums" level="2" php="8.1">
             Enums are built-in in the language:
 
-            <pre><code class="language-php hljs php"><hljs keyword>enum</hljs> <hljs type>Status</hljs>
+            <pre><code class="language-php hljs php"><span class="hljs-highlight keyword">enum</span> <span class="hljs-highlight type">Status</span>
  {
-     case <hljs prop>DRAFT</hljs>;
-     case <hljs prop>PUBLISHED</hljs>;
-     case <hljs prop>ARCHIVED</hljs>;
+     case <span class="hljs-highlight prop">DRAFT</span>;
+     case <span class="hljs-highlight prop">PUBLISHED</span>;
+     case <span class="hljs-highlight prop">ARCHIVED</span>;
  }</code></pre>
 
         </x-card>
 
-        <x-card id="enums-methods" title="Enums methods" level="2" php="8.1">
+        <x-card id="enum-methods" title="Enum methods" level="2" php="8.1">
             Enums can have methods, as well as have a string or integer value per case:
 
-            <pre><code class="language-php hljs php"><hljs keyword>enum</hljs> <hljs type>Status</hljs>: <hljs type>int</hljs>
+            <pre><code class="language-php hljs php"><span class="hljs-highlight keyword">enum</span> <span class="hljs-highlight type">Status</span>: <span class="hljs-highlight type">int</span>
  {
-     case <hljs prop>DRAFT</hljs> = 1;
-     case <hljs prop>PUBLISHED</hljs> = 2;
-     case <hljs prop>ARCHIVED</hljs> = 3;
+     case <span class="hljs-highlight prop">DRAFT</span> = 1;
+     case <span class="hljs-highlight prop">PUBLISHED</span> = 2;
+     case <span class="hljs-highlight prop">ARCHIVED</span> = 3;
 
      public function color(): string
      {
-         return <hljs keyword>match</hljs>($this)
+         return <span class="hljs-highlight keyword">match</span>($this)
          {
-             <hljs type>Status</hljs>::<hljs prop>DRAFT</hljs> => 'grey',
-             <hljs type>Status</hljs>::<hljs prop>PUBLISHED</hljs> => 'green',
-             <hljs type>Status</hljs>::<hljs prop>ARCHIVED</hljs> => 'red',
+             <span class="hljs-highlight type">Status</span>::<span class="hljs-highlight prop">DRAFT</span> => 'grey',
+             <span class="hljs-highlight type">Status</span>::<span class="hljs-highlight prop">PUBLISHED</span> => 'green',
+             <span class="hljs-highlight type">Status</span>::<span class="hljs-highlight prop">ARCHIVED</span> => 'red',
          };
      }
  }</code></pre>
